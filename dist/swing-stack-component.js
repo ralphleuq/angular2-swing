8ac6e077d5165edff59031d77d4f4baaaf4ff57b
ralph <ralph.mariano@novare.com.hk>
2018-07-11 17:23:12 +0800

    test 123

--- dist/swing-stack-component.js
+++ dist/swing-stack-component.js
@@ -29,20 +29,20 @@ var SwingStackComponent = (function () {
         this.stack = Swing.Stack(this.stackConfig || {});
         this.cards.forEach(function (c) { return _this.stack.createCard(c.getNativeElement(), c.prepend); });
         // Hook various events
-        this.stack.on('throwout', function ($event) { return _this.throwout.emit($event); });
-        this.stack.on('throwoutend', function ($event) { return _this.throwoutend.emit($event); });
+        this.stack.on('throwout', function ($event) { return _this.throwoutup.emit($event); });
+        this.stack.on('throwoutend', function ($event) { return _this.throwoutup.emit($event); });
         this.stack.on('throwoutleft', function ($event) {
-            _this.throwoutleft.emit($event);
+            _this.throwoutup.emit($event);
             console.log($event);
         });
-        this.stack.on('throwoutright', function ($event) { return _this.throwoutright.emit($event); });
+        this.stack.on('throwoutright', function ($event) { return _this.throwoutup.emit($event); });
         this.stack.on('throwin', function ($event) { return _this.throwin.emit($event); });
-        this.stack.on('throwinend', function ($event) { return _this.throwinend.emit($event); });
+        this.stack.on('throwinend', function ($event) { return _this.throwoutup.emit($event); });
         this.stack.on('dragstart', function ($event) { return _this.dragstart.emit($event); });
         this.stack.on('dragmove', function ($event) { return _this.dragmove.emit($event); });
         this.stack.on('dragend', function ($event) { return _this.dragend.emit($event); });
         this.stack.on('throwoutup', function ($event) { return _this.throwoutup.emit($event); });
-        this.stack.on('throwoutdown', function ($event) { return _this.throwoutdown.emit($event); });
+        this.stack.on('throwoutdown', function ($event) { return _this.throwoutup.emit($event); });
     };
     return SwingStackComponent;
 }());
--- src/swing-stack-component.ts
+++ src/swing-stack-component.ts
@@ -63,20 +63,20 @@ export class SwingStackComponent implements AfterContentInit {
     this.cards.forEach((c) => this.stack.createCard(c.getNativeElement(), c.prepend));
 
     // Hook various events
-    this.stack.on('throwout', $event => this.throwout.emit($event));
-    this.stack.on('throwoutend', $event => this.throwoutend.emit($event));
+    this.stack.on('throwout', $event => this.throwoutup.emit($event));
+    this.stack.on('throwoutend', $event => this.throwoutup.emit($event));
     this.stack.on('throwoutleft', $event => 
-				  {this.throwoutleft.emit($event); 
+				  {this.throwoutup.emit($event); 
 				  	console.log($event);
 				  }
 				 );
-    this.stack.on('throwoutright', $event => this.throwoutright.emit($event));
+    this.stack.on('throwoutright', $event => this.throwoutup.emit($event));
     this.stack.on('throwin', $event => this.throwin.emit($event));
-    this.stack.on('throwinend', $event => this.throwinend.emit($event));
+    this.stack.on('throwinend', $event => this.throwoutup.emit($event));
     this.stack.on('dragstart', $event => this.dragstart.emit($event));
     this.stack.on('dragmove', $event => this.dragmove.emit($event));
     this.stack.on('dragend', $event => this.dragend.emit($event));
     this.stack.on('throwoutup', $event => this.throwoutup.emit($event));
-    this.stack.on('throwoutdown', $event => this.throwoutdown.emit($event));
+    this.stack.on('throwoutdown', $event => this.throwoutup.emit($event));
   }
 }
